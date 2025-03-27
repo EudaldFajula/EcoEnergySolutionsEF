1880f9b4fe0ab9e6d4691b82dd63f7da4018bb43
Eudald Fajula <eudald.fajula.7e6@itb.cat>
2025-03-27 23:32:43 +0100

    Creat DbContext. Closes #8

---
+++ EcoEnergySolutionsEF/data/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using EcoEnergySolutionsEF.Classes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EcoEnergySolutionsEF.data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Simulacio> Simulacio { get; set; }
+        public DbSet<EnergyIndicator> EnergyIndicator { get; set; }
+        public DbSet<WaterConsumption> WaterConsumption { get; set; }
+
+        //Constructor necessari per injecció de dependencies
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+            options.UseSqlServer(connectionString);
+        }
+    }
+}
